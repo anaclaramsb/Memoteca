afe1b218203981a03f2d7d481699168757d31ce2
Ana Clara Batista <50646420+anaclaramsb@users.noreply.github.com>
2024-08-15 11:00:54 -0300

    aula 7

---
+++ src/app/componentes/pensamentos/criar-pensamento/minusculaValidator.ts
@@ -0,0 +1,7 @@
+import { AbstractControl } from '@angular/forms';
+
+export function minusculoValidator(control: AbstractControl) {
+  const autoria = control.value as string;
+  if (autoria !== autoria?.toLowerCase()) return { minusculo: true };
+  else return null;
+}
